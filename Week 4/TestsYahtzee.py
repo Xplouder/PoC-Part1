342ff18da4a8789c5cdf138f489d6124447d82a3
João Silva <jfasxx@hotmail.com>
2016-02-04 15:49:43 +0000

    readability improvement :ok_hand:

--- Week 4/TestsYahtzee.py
+++ Week 4/TestsYahtzee.py
@@ -7,82 +7,86 @@ import Yahtzee
 
 class TestsYahtzee(unittest.TestCase):
     def test_expected_value1(self):
-        val = Yahtzee.expected_value((2, 2), 6, 2)
-        self.assertAlmostEqual(5.83333333333, val)
+        expected_result = Yahtzee.expected_value((2, 2), 6, 2)
+        self.assertAlmostEqual(5.83333333333, expected_result)
 
     def test_expected_value2(self):
-        val = Yahtzee.expected_value((2, 2), 2, 2)
-        self.assertAlmostEqual(6, val)
+        expected_result = Yahtzee.expected_value((2, 2), 2, 2)
+        self.assertAlmostEqual(6, expected_result)
 
     def test_gen_all_holds1(self):
-        val = Yahtzee.gen_all_holds((1,))
-        self.assertEqual({(), (1,)}, val)
+        expected_result = Yahtzee.gen_all_holds((1,))
+        self.assertEqual({(), (1,)}, expected_result)
 
     def test_gen_all_holds2(self):
-        val = Yahtzee.gen_all_holds((5, 5))
-        self.assertEqual({(5,), (), (5, 5)}, val)
+        expected_result = Yahtzee.gen_all_holds((5, 5))
+        self.assertEqual({(5,), (), (5, 5)}, expected_result)
 
     def test_gen_all_holds3(self):
-        val = Yahtzee.gen_all_holds((4, 6))
-        self.assertEqual({(4, 6), (), (6,), (4,)}, val)
+        expected_result = Yahtzee.gen_all_holds((4, 6))
+        self.assertEqual({(4, 6), (), (6,), (4,)}, expected_result)
 
     def test_gen_all_holds4(self):
-        val = Yahtzee.gen_all_holds((1, 3, 5))
+        expected_result = Yahtzee.gen_all_holds((1, 3, 5))
         self.assertEqual(
-            {(1, 3), (1,), (3,), (1, 3, 5), (1, 5), (5,), (), (3, 5)}, val)
+            {(1, 3), (1,), (3,), (1, 3, 5), (1, 5), (5,), (), (3, 5)},
+            expected_result)
 
     def test_gen_all_holds5(self):
-        val = Yahtzee.gen_all_holds((2, 2, 4))
-        self.assertEqual({(), (2, 2), (2,), (2, 2, 4), (2, 4), (4,)}, val)
+        expected_result = Yahtzee.gen_all_holds((2, 2, 4))
+        self.assertEqual({(), (2, 2), (2,), (2, 2, 4), (2, 4), (4,)},
+                         expected_result)
 
     def test_gen_all_holds6(self):
-        val = Yahtzee.gen_all_holds((1, 1, 2, 2))
+        expected_result = Yahtzee.gen_all_holds((1, 1, 2, 2))
         self.assertEqual(
             {(1, 2), (1, 1, 2, 2), (1,), (1, 2, 2),
-             (2,), (1, 1, 2), (), (2, 2), (1, 1)}, val)
+             (2,), (1, 1, 2), (), (2, 2), (1, 1)}, expected_result)
 
     def test_gen_all_holds7(self):
-        val = Yahtzee.gen_all_holds((2, 4, 6, 6))
+        expected_result = Yahtzee.gen_all_holds((2, 4, 6, 6))
         self.assertEqual(
             {(2, 6), (4, 6), (6, 6), (2,), (2, 4, 6), (4,), (2, 4, 6, 6), (),
-             (6,), (2, 6, 6), (4, 6, 6), (2, 4)}, val)
+             (6,), (2, 6, 6), (4, 6, 6), (2, 4)}, expected_result)
 
     def test_gen_all_holds8(self):
-        val = Yahtzee.gen_all_holds(tuple([]))
-        self.assertEqual({()}, val)
+        expected_result = Yahtzee.gen_all_holds(tuple([]))
+        self.assertEqual({()}, expected_result)
 
     def test_gen_all_holds9(self):
-        val = Yahtzee.gen_all_holds(tuple([2, 4]))
-        self.assertEqual({(), (2,), (4,), (2, 4)}, val)
+        expected_result = Yahtzee.gen_all_holds(tuple([2, 4]))
+        self.assertEqual({(), (2,), (4,), (2, 4)}, expected_result)
 
     def test_gen_all_holds10(self):
-        val = Yahtzee.gen_all_holds(tuple((3, 3, 3)))
-        self.assertEqual({(), (3,), (3, 3), (3, 3, 3)}, val)
+        expected_result = Yahtzee.gen_all_holds(tuple((3, 3, 3)))
+        self.assertEqual({(), (3,), (3, 3), (3, 3, 3)}, expected_result)
 
     def test_gen_all_holds11(self):
-        val = Yahtzee.gen_all_holds(tuple((1, 2, 2)))
-        self.assertEqual({(), (1,), (2,), (1, 2), (2, 2), (1, 2, 2)}, val)
+        expected_result = Yahtzee.gen_all_holds(tuple((1, 2, 2)))
+        self.assertEqual({(), (1,), (2,), (1, 2), (2, 2), (1, 2, 2)},
+                         expected_result)
 
     def test_gen_all_holds12(self):
-        val = Yahtzee.gen_all_holds(tuple([2, 3, 6]))
+        expected_result = Yahtzee.gen_all_holds(tuple([2, 3, 6]))
         self.assertEqual(
-            {(), (2,), (3,), (6,), (2, 3), (2, 6), (3, 6), (2, 3, 6)}, val)
+            {(), (2,), (3,), (6,), (2, 3), (2, 6), (3, 6), (2, 3, 6)},
+            expected_result)
 
     def test_score1(self):
-        val = Yahtzee.score((1,))
-        self.assertEqual(1, val)
+        expected_result = Yahtzee.score((1,))
+        self.assertEqual(1, expected_result)
 
     def test_score2(self):
-        val = Yahtzee.score((4, 5))
-        self.assertEqual(5, val)
+        expected_result = Yahtzee.score((4, 5))
+        self.assertEqual(5, expected_result)
 
     def test_score3(self):
-        val = Yahtzee.score((3, 3, 3))
-        self.assertEqual(9, val)
+        expected_result = Yahtzee.score((3, 3, 3))
+        self.assertEqual(9, expected_result)
 
     def test_strategy(self):
-        val = Yahtzee.strategy((1,), 6)
-        self.assertEqual((3.5, ()), val)
+        expected_result = Yahtzee.strategy((1,), 6)
+        self.assertEqual((3.5, ()), expected_result)
 
 
 if __name__ == '__main__':
